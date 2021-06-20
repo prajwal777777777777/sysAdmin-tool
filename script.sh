b5d6cb5e6e26b52e01d868cf637d007953ae96c6
prajwal <prajwal@localhost.localdomain>
2021-06-19 20:21:58 -0700

    new commit

--- script.sh
+++ script.sh
@@ -35,6 +35,7 @@ fi
 function filecheck() {
 local line=0
 global_file_found=0
+quit_symbol=0
 
 			while (( 0!=1 ))
 			do
@@ -55,8 +56,8 @@ global_file_found=0
 							
 				elif [[ $file_checks == "q" ]] || [[ $file_checks == "quit" ]]
 					then
+						quit_symbol=$[ quit_symbol + 1 ]
 						break;
-						
 				else
 					seprator;
 						center "The give file doesnot seems to exist!!!"
@@ -176,6 +177,11 @@ function compression {
 			1|"archive")
 				filecheck;
 				seprator;
+				echo "";
+				if [[ $quit_symbol -eq 1 ]] 
+				then
+					break
+				fi;
 				center "Compressing the file"
 				tar -cvf $basename.tar -C $dirname $basename > /dev/null 2>&1
 				command_status
@@ -188,8 +194,10 @@ function compression {
 			2|"gzip")
 				filecheck;
 				seprator;
+				echo ""
+				if [[ $quit_symbol -eq 1 ]];then break;fi
 				center "Compressing the file"
-				tar -cvzf $basename.tar.gz $file_checks > /dev/null 2>&1
+				tar -cvzf $basename.tar.gz -C $dirname $basename > /dev/null 2>&1
 				command_status
 				if [[ $sucess -eq 1 ]]
 				then
@@ -198,10 +206,12 @@ function compression {
 				fi;;
 		  
 			3|"zip"|"bz2")
-				filecheck;
+				filecheck;	
 				seprator;
+				echo "";
+				if [[ $quit_symbool -eq 1 ]];then break;fi
 				center "Compressing the file"
-				tar -cvjf $basename.tar.bz2 $file_checks > /dev/null 2>&1
+				tar -cvjf $basename.tar.bz2 -C $dirname $basename > /dev/null 2>&1
 				command_status;
 				if [[ $sucess -eq 1 ]]
 				then
@@ -212,18 +222,33 @@ function compression {
 			4|"xz")
 				filecheck;
 				seprator;
+				echo ""
+				if [[ $quit_symbol -eq 1 ]];then break;fi
 				center "Compressing the file"
-				tar -cvJf $basename.xz $file_checks > /dev/null 2>&1
+				tar -cvJf $basename.xz -C $dirname $basename > /dev/null 2>&1
 				command_status;
 				if [[ $sucess -eq 1 ]]
 				then
 					center "xz file created on $dirname/$basename.xz"
 					seprator
 				fi;;
-
-			7|"extract")
-				filecheck;
+	
+			5|"view")
+				filecheck;	
 				seprator;
+				echo "";
+				if [[ $quit_symbol -eq 1 ]];then break;fi
+				center "list of files"
+				tar -tvf $file_checks 
+				seprator;;
+
+			
+
+			6|"extract")
+				filecheck;	
+				seprator;
+				echo ""
+				if [[ $quit_symbol -eq 1 ]];then break;fi
 				center "Extracting the file"
 				tar -xvf $file_checks > /dev/null 2>&1
 				command_status;
@@ -232,10 +257,102 @@ function compression {
 					center "Files has been sucessfully extracted on $dirname."
 					seprator
 				fi;;
-			16|"back")
+
+		
+			7|"7z")
+				filecheck;	
+				seprator;
+				echo "";
+				if [[ $quit_symbol -eq 1 ]];then break;fi
+				center "Compressing the file"
+				7z a $basename.7z $file_checks > /dev/null 2>&1
+				command_status;
+				if [[ $sucess -eq 1 ]]
+				then
+					center "File has been sucessfully compressed!!"
+					seprator
+				fi;;
+		
+		  8|"extract7z")
+				filecheck;
+				seprator;
+				echo ""
+				if [[ $quit_symbol -eq 1 ]];then break;fi
+				center "Extracting the file...."
+				7z e $file_checks -o$dirname/$basename.extracted > /dev/null 2>&1
+				command_status;
+				if [[ $sucess -eq 1 ]]
+				then 
+					center "File has been sucessfully extracted!!!"
+					seprator;
+				fi;;
+
+			9|"view7z")
+				filecheck;
+				seprator;
+				echo ""
+				if [[ $quit -eq 1 ]];then break;fi
+				center "List of files"
+				echo ""
+				7z l $file_checks
+				echo ""
+				seprator;;
+
+			10|"test")
+			filecheck;
+			seprator;
+			echo ""
+			if [[ $quit -eq 1 ]];then break;fi
+			center "Intergrity"
+			echo ""
+			7z t $file_checks
+			echo ""
+			seprator;;
+
+		11|"password")
+			filecheck;
+			seprator;
+			echo ""
+			if [[ $quit -eq 1 ]];then break;fi
+			read -p "Enter the password:" -s password
+			7z a $basename.7z $file_checks -p"$password"
+			command_status;
+			if [[ $sucess -eq 1 ]]
+			then
+				center "file has been sucessfully created with password created!"
+				seprator
+			fi;;
+
+		12|"viewrar")
+			filecheck;
+			seprator;
+			echo ""
+			if [[ $quit -eq 1 ]];then break;fi
+			rar l $filechecks
+			command_status;
+			if [[ $sucess -eq 1 ]]
+			then 
+				center "Contents of files"
+				seprator;
+			fi;;
+
+		13|"createrar")
+			filecheck;
+			seprator;
+			echo ""
+			if [[ $quit -eq 1 ]];then break;fi
+			center "compressing the file"
+			rar a $basename.rar $filechecks > /dev/null 2>&1
+			if [[ $status -eq 1 ]]
+			then 
+				echo "File has been compressed sucessfully"
+			fi;;
+
+		  
+		16|"back")
 		 		case_option;;
 
-			17|"exit"|"quit")
+			18|"exit"|"quit")
 			center "Bye"
 			seprator;
 			exit;;
@@ -247,6 +364,7 @@ function compression {
 		
 		esac
 	done	
+compression;
 }
 
 
@@ -255,20 +373,18 @@ function case_option
 local flag=0
 clear
 
-while (( $flag!=1 )) 
+while (( 0!=1 )) 
 do
 	cat show_file	
 	read -p "Enter option# " option
 	case $option in
 		1|"basic system lookup"|"Basic System Lookup")
 			clear;
-			system_lookup
-			$((flag++));;
+			system_lookup;;
 		
 		2|"Compression"|"compression"|"COMPRESSION" )
 			clear;
 			compression;
-			$((flag++))
 			;;		
 		
 		3|"hardware lookup"|"Hardware lookup")
